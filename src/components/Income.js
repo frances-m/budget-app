15f5c5af2cd7b93c19b26ccfea3180892c9620f1
francesm <fr.ncesm@gmail.com>
2022-08-08 20:07:12 -0600

    results and expenses components added & results styling

--- src/components/Income.js
+++ src/components/Income.js
@@ -1,16 +1,16 @@
 const Income = () => {
 
     return (
-        <section class="income">
+        <section className="income">
             <div className="wrapper">
                 <h2>Income</h2>
                 <form action="">
-                    <div class="input-container">
+                    <div className="input-container">
                         <label htmlFor="cat1">category</label>
                         <input type="text" name="cat1" id="cat1" />
                     </div>
 
-                    <div class="input-container">
+                    <div className="input-container">
                         <label htmlFor="cat2">category</label>
                         <input type="text" name="cat2" id="cat2" />
                     </div>
