d0345b46cd2eef715ddf45a48147cb553929673f
francesm <fr.ncesm@gmail.com>
2022-08-08 19:56:10 -0600

    add income component and input/form styling

--- src/components/Income.js
+++ src/components/Income.js
@@ -1,7 +1,22 @@
 const Income = () => {
 
     return (
-        <></>
+        <section class="income">
+            <div className="wrapper">
+                <h2>Income</h2>
+                <form action="">
+                    <div class="input-container">
+                        <label htmlFor="cat1">category</label>
+                        <input type="text" name="cat1" id="cat1" />
+                    </div>
+
+                    <div class="input-container">
+                        <label htmlFor="cat2">category</label>
+                        <input type="text" name="cat2" id="cat2" />
+                    </div>
+                </form>
+            </div>
+        </section>
     )
 }
 
