f301a3c27132264bdec64ec3fe58015d3ab9d4bd
francesm <fr.ncesm@gmail.com>
2022-10-24 14:11:35 -0600

    update firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyB-o8PxHRFGPKa4OGfUu-POWiKE8J5AfDg",
+    authDomain: "budget-app-a0353.firebaseapp.com",
+    databaseURL: "https://budget-app-a0353-default-rtdb.firebaseio.com",
+    projectId: "budget-app-a0353",
+    storageBucket: "budget-app-a0353.appspot.com",
+    messagingSenderId: "1800492586",
+    appId: "1:1800492586:web:11b9d7cd95f9079984d553"
 };
 
 // Initialize Firebase
